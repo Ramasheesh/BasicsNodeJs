bed48a3ea36c1af9873ee62c26829606e7349a2a
RamAsheesh Chauhan <rch231198@gmail.com>
2023-04-15 12:13:17 +0530

    server created

--- Basics/createServer.js
+++ Basics/createServer.js
@@ -1,6 +1,44 @@
-const http = require('http')
+const express = require('express')
+const createS = express()
 
-http.createServer((req,res)=>{
-    res.send("helo sir ");
-    // res.end("done");
-}).listen(1000);
+createS.get('/',(req,res)=>{
+    res.send("<h1>Hello sir I'm RamAsheesh</h1>");
+}).listen(4000,()=>{
+    console.log("server is running at port,4000")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Create server
+// http.createServer(function (req, res) {
+
+// 	// Send the HTTP header
+// 	// HTTP Status: 200 : OK
+// 	// Content Type: text/plain
+// 	res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+// 	// Send the response body as "This is the example
+// 	// of node.js web based application"
+// 	res.end('This is the example of node.js web-based application \n');
+
+// 	// Console will display the message
+// }).listen(5000,
+// 	() => console.log('Server running at http://127.0.0.1:5000/'));
+
