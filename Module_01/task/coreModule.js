9dafd8e552d067bc691c1f255e4c8398067a66f7
RamAsheesh <rch231198@gmail.com>
2023-07-06 21:52:55 +0530

    change done

---
+++ Module_01/task/coreModule.js
@@ -0,0 +1,14 @@
+// const fs = require("fs")
+// // fs.writeFileSync("hello.pdf")
+// fs.writeFileSync('"h1.txt"')
+// console.log("error")
+// console.log(__dirname);
+// console.log(__filename);
+// // fs.writeFileSync("hell.txt")
+
+// // fs.appendFile()
+// // fs.open()
+// // fs.writeFile()
+
+// const gs = require('fs').writeFileSync;
+// gs("hello.txt");
